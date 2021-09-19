47df350ffab8524330db83686f67d0699c2d3792
chma19063@g.nihon-u.ac.jp <chma19063@g.nihon-u.ac.jp>
2021-09-19 12:37:30 +0900

    fixed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,14 +3,47 @@ const Main = () => {
 
     return(
     <section className = "section">
-
         <article class="message is-success">
             <div class="message-header">
                 <p>おうち時間で簡単知育ゲーム </p>
             </div>
 
             <div class="message-body">
-         
+               対象ユーザーは小さなお子様や高齢者。 <br/>
+               ゲーム感覚で野菜を学習できる。<br/>
+               成果が分かりやすいので続けられる。<br/>
+
+               <br/>
+            
+
+               また、高齢者に対しては操作が簡単だから、ゲームを楽しめることができる。<br/>
+               脳トレで認知症予防ができる。
+            </div>
+        </article>
+        
+        <article class="message is-success">
+            <div class="message-header">
+                <p>ゲーム説明</p>
+            </div>
+
+            <div class="message-body">
+                ベジカルにはカルタのモードとかさねえのモードという二つのモードがあります。
+                さらに、カルタのモードでは野菜の名前モードと野菜の説明モードがあります。
+
+                <br/>
+                <br/>
+
+                まず、カルタのモードでは画面上に野菜の名前や説明が表示されます。そして、その表示に合う野菜のカルタを
+                クリックすることでカルタが消えていき、カルタが全てなくなると終了となります。
+                また、ゲームが終了するとスコア表示され、さらに上位5つのスコアが表示されます。
+
+                <br/>
+                <br/>
+
+                次に、かさねえモードでは4つの野菜が半透明となって重なった状態で表示されます。
+                ゲームをプレイしているユーザーがその重なっている野菜を当てていきます。
+                そして、数秒後には答えの野菜が表示されます。
+                ゲームの難易度はやさしいとむずかしいがあり、それぞれ野菜が表示される時間が異なります。
             </div>
         </article>
         
@@ -34,9 +67,9 @@ const Main = () => {
         
          
                 <div className="column">
-                    <a href="https://drive.google.com/file/d/1x44Mvxm6P1Pd3zSJtMVHf3wk7wrGZILk/view" target="_blank">
+                    <a href="https://docs.google.com/presentation/d/e/2PACX-1vT7MIN7xf8zNN-tX6p_R7B5PagvXpHN2noPe89hqEuZJvJMpd714--D_v7QlYkLpZr78GKfFKsgxulz/pub?start=false&loop=false&delayms=3000&slide=id.p" target="_blank">
                         <div className = "button is-large hero is-success">
-                            <p className="title is-white">企画書</p>
+                            <p className="title is-white">スライド</p>
                         </div>    
                     </a>
                 </div>  
